a21c026c4e6169723d1e8824adb207c27b61bab8
tamiib <tamibarisic28@gmail.com>
2022-01-11 00:12:22 +0100

    added frontend

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
+
+function App() {
+  return  (
+    <div className="center">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
